a1e8402b9251c4b976074199652812a933d7cacb
“Ali <tadayoni.ali.enayat@gmail.com>
2024-09-12 13:38:59 +0330

    add supabase database to the project

---
+++ src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not bbe loaded");
+  }
+  return data;
+}
