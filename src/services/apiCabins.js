38ba03698651ece3080419838463037afccd72f4
“Ali <tadayoni.ali.enayat@gmail.com>
2024-09-17 07:31:49 +0330

    add delete cabin feature

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,7 +5,17 @@ export async function getCabins() {
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could not bbe loaded");
+    throw new Error("Cabins could not b e loaded");
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
   }
   return data;
 }
