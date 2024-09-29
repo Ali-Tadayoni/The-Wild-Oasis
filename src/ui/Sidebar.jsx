a8ef50e4257085356f6f9acf2fce6593e7601e8e
“Ali <tadayoni.ali.enayat@gmail.com>
2024-09-29 08:31:57 +0330

    add some fake data to work with

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import MainNav from "./MainNav";
 import Logo from "./Logo";
+import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -18,6 +19,7 @@ function Sidebar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
+      <Uploader />
     </StyledSidebar>
   );
 }
