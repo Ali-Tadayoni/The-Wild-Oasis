cea6a5ae93e25edaf7f71c2e896ca16cb41e0151
“Ali <tadayoni.ali.enayat@gmail.com>
2024-10-02 02:44:52 +0330

    implement authorization protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Settings from "./pages/Settings";
 import PageNotFound from "./pages/PageNotFound";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,7 +33,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -8,7 +8,16 @@ export async function login({ email, password }) {
 
   if (error) throw new Error(error.message);
 
-  console.log(data);
-
   return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
 }
