1105a5e558d516460dfb101bf48e88de602d80e1
“Ali <tadayoni.ali.enayat@gmail.com>
2024-10-03 12:51:53 +0330

    building the App Header

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -6,7 +6,6 @@ export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
     onSuccess: (user) => {
-      console.log(user);
       toast.success(
         "Account successfully created! Please verify the new account from user's email address."
       );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,24 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+
+import HeaderMenu from "./HeaderMenu";
+import UserAvatr from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatr />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
