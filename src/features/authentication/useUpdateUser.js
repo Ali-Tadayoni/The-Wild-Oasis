363258f58db7cfb5a52d1c66798d6ba913c2d183
“Ali <tadayoni.ali.enayat@gmail.com>
2024-10-03 16:37:31 +0330

    implement dark mode

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -11,9 +11,7 @@ export function useUpdateUser() {
     onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
 
-      queryClient.setQueryData("user", user);
-
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueryData(["user"], user);
     },
     onError: (err) => toast.error(err.message),
   });
