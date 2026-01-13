f730a426e6fe273b5d0ce2cb325ed85c75353fb5
“Ali <tadayoni.ali.enayat@gmail.com>
2026-01-13 16:01:52 +0330

    refactor: sort imports, optimize code

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -12,7 +12,6 @@ const StyledUserAvatar = styled.div`
 
 const Avatar = styled.img`
   display: block;
-  width: 4rem;
   width: 3.6rem;
   aspect-ratio: 1;
   object-fit: cover;
@@ -26,10 +25,7 @@ function UserAvatar() {
   const { fullName, avatar } = user.user_metadata;
   return (
     <StyledUserAvatar>
-      <Avatar
-        src={avatar || "default-user.jpg"}
-        alt={`Avatar of ${fullName}`}
-      />
+      <Avatar src={avatar || "default-user.jpg"} alt={`Avatar of ${fullName}`} />
       <span>{fullName}</span>
     </StyledUserAvatar>
   );
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
@@ -61,9 +62,7 @@ const PaginationButton = styled.button`
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const currentPage = !searchParams.get("page")
-    ? 1
-    : Number(searchParams.get("page"));
+  const currentPage = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   const pageCount = Math.ceil(count / PAGE_SIZE);
 
@@ -87,10 +86,8 @@ function Pagination({ count }) {
     <StyledPagination>
       <P>
         Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
-        <span>
-          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
-        </span>{" "}
-        of <span>{count}</span> results
+        <span>{currentPage === pageCount ? count : currentPage * PAGE_SIZE}</span> of{" "}
+        <span>{count}</span> results
       </P>
 
       <Buttons>
@@ -98,10 +95,7 @@ function Pagination({ count }) {
           <HiChevronLeft /> <span>Previous</span>
         </PaginationButton>
 
-        <PaginationButton
-          onClick={nextPage}
-          disabled={currentPage === pageCount}
-        >
+        <PaginationButton onClick={nextPage} disabled={currentPage === pageCount}>
           <span>Next</span>
           <HiChevronRight />
         </PaginationButton>
